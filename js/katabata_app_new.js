27e136216b91987890c4eab3ae52c4a9654d2896
wolfsworld <wolf@wolfsworld1.com>
2016-02-06 13:33:51 -0500

    uyiyi

--- js/katabata_app_new.js
+++ js/katabata_app_new.js
@@ -37,7 +37,7 @@ function onStatus(status){
 		//}
 }
 
-alert(exercises[0].Media.status);
+alert('this is'+exercises[0]+'');
 //var my_media=new Media("/android_asset/www/sounds/go.mp3");
 //my_media.play();
 
